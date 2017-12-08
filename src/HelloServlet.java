fa8db84c789e7e10784ea1e794b47d81b43d00da
IceCodeBrain <925234379@qq.com>
2017-12-08 10:44:27 +0800

    test

---
+++ src/HelloServlet.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		String sta = arg0.getParameter("sta");
+		System.out.println(sta);
+		arg0.getRequestDispatcher("NewFile.jsp").forward(arg0, arg1);
+	}
+
+}
